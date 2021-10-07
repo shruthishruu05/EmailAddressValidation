ee90386b11fa21d87d43dc59312ec91e247f4830
shruthishruu05 <shruthi.m@perfios.com>
2021-10-07 21:04:26 +0530

    REFACTORED : to validate the 2nd mandatory domain name

--- EmailAddressValidation.js
+++ EmailAddressValidation.js
@@ -4,5 +4,14 @@ arguments.splice(0,2);
 let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])");
 arguments.forEach(element => 
     {
-    console.log(element+": ", emailRegex.test(element));
+    console.log("uc1" +element+": ", emailRegex.test(element));
+});
+
+ arguments = process.argv;
+arguments.splice(0,2);
+
+emailRegex = RegExp("^[a-zA-Z]*[@][.a-zA-z]*$");
+arguments.forEach(element => 
+    {
+    console.log("uc2" +element+": ", emailRegex.test(element));
 });
