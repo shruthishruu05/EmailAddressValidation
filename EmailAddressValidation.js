82b5f3e615d6d226008b7e800ce7cc4fb7fa10c8
shruthishruu05 <shruthi.m@perfios.com>
2021-10-07 20:58:15 +0530

    ADDED : validation of mandatory part of the email

--- EmailAddressValidation.js
+++ EmailAddressValidation.js
@@ -2,6 +2,7 @@ let arguments = process.argv;
 arguments.splice(0,2);
 
 let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])");
-arguments.forEach(element => {
+arguments.forEach(element => 
+    {
     console.log(element+": ", emailRegex.test(element));
 });
